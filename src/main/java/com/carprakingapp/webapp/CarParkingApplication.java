3cda11c84af520072a59b3ed4b614a0839875920
hnmnjrkr01 <heenajava10@gmail.com>
2024-12-18 11:19:11 -0600

    POM basic configuration

--- src/main/java/com/carprakingapp/webapp/CarParkingApplication.java
+++ src/main/java/com/carprakingapp/webapp/CarParkingApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarParkingApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CarParkingApplication.class, args);
+
+		System.out.println("====Welcome To Car Parking Application====");
 	}
 
 }
